0cfa00e502158e8535e07af1e837e58e0f42d7e0
pingtougea <1465164225@qq.com>
2025-10-28 19:23:17 +0800

    git commit -m "fix: remove json comments and fix vite config"

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: './',
-//   build: {
-//     outDir: 'dist',
-//     emptyOutDir: true
-//   },
-//   resolve: {
-//     extensions: ['.js', '.jsx', '.ts', '.tsx']
-//   },
-//   server: {
-//     host: true
-//   }
-// })
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          utils: ['lodash', 'axios']
+          antd: ['antd']
         }
       }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
     }
   }
 })
